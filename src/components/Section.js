c439452f8eeb3607fa182517b9d361a4d6e38155
Jalaj Jain <jalajjain39@gmail.com>
2021-05-16 12:09:22 +0530

    tesla-clone 1.0

---
+++ src/components/Section.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import styled from "styled-components";
+import Fade from 'react-reveal/Fade';
+
+function Section(props) {
+    
+    return (
+        <Wrap bgImage={props.backgroundImg}>
+        <Fade bottom>
+            <ItemText>
+                <h1>{props.title}</h1>
+                <p>{props.description}</p>
+            </ItemText>
+            </Fade>
+            <Buttons>
+            <Fade bottom>
+            <ButtonGroup>
+               <LeftButton>
+                   {props.leftBtnText}
+               </LeftButton>
+               {props.rightBtnText && 
+                <RightButton>
+                  {props.rightBtnText}
+               </RightButton>
+               }
+               
+            </ButtonGroup>
+            </Fade>
+            <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div `
+z-index:10;
+display:flex;
+flex-direction:column;
+width:100vw;
+height:100vh;
+background-position:center;
+background-size:cover;
+background-repeat:no-repeat;
+background-image: url('/images/model-s.jpg');
+justify-content:space-between; 
+align-items:center;
+background-image:${props =>`url("/images/${props.bgImage}")`}
+`
+const ItemText = styled.div `
+padding-top:35%;
+text-align:center;
+z-index:-1;
+`
+const ButtonGroup = styled.div `
+ display:flex;
+ margin-bottom:30px;
+ @media (max-width:768px){
+     flex-direction:column;
+ }
+`
+const LeftButton =styled.div `
+background-color:rgba(23,26,32,0.8);
+height:40px;
+width:260px;
+color:white;
+display:flex;
+justify-content:center;
+align-items:center;
+border-radius:100px;
+opacity:0.85;
+text-tranfoem:uppercase;
+font-size:12px;
+cursor:pointer;
+margin:8px;
+`
+const RightButton =styled(LeftButton)`
+background-color:white;
+opacity:0.65;
+color:black;
+`
+const DownArrow = styled.img `
+position:relative;
+left:45%;
+height:40px;
+overflow-x:hidden;
+animation: animateDown infinite 1.5s;
+`
+const Buttons = styled.div `
+
+`
